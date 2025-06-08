ffa647e1ef10d155c70d185b21a17784095223ae
NikitaKharzheev <kharzheev1999@mail.ru>
2025-06-08 17:36:57 +0300

    Added poetry

--- app/llm_wrapper.py
+++ app/llm_wrapper.py
@@ -6,7 +6,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 def call_llm(query, project_context):
-    course_api_key = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI2N2E4YTY5ZGU5OTI3Yjg1N2Q1YTYzOTgiLCJleHAiOjE3NTQ2MTEyMDB9.QqqmLjBXJXntJEaFe1w8QHj18jdVKBCeqErJ2kgBhuU'
+    course_api_key = '...'
     llm = ChatOpenAI(temperature=0.0, course_api_key=course_api_key)
 
     prompt = (
